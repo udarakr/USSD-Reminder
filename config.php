4c9157000507a7c0e54a286b043c026f8aa8294a
Udara Rathnayake <udarakr@gmail.com>
2015-05-22 14:34:27 +0530

    adding configurations

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $USSD = new stdClass();
 
-$USSD->host='';
-$USSD->db_user='';
+$USSD->host='localhost';
+$USSD->db_user='root';
 $USSD->db_name='tcreminder';
-$USSD->db_password='';
+$USSD->db_password='123';
 
 
 $USSD->http_client='';
